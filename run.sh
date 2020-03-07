4896163089097d4a986fd40f3716c112f32b6635
ody5sey <60533474+ody5sey@users.noreply.github.com>
2020-03-07 08:43:52 +0800

    修复dirsearch的docker

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ pipenv install
 # 下载所需的镜像
 docker pull ap0llo/oneforall:0.0.9
 docker pull ap0llo/nmap:7.80
-docker pull ap0llo/dirsearh:0.3.9
+docker pull ap0llo/dirsearch:0.3.9
 docker pull ap0llo/poc:xunfeng
 docker pull ap0llo/poc:kunpeng
 docker pull mongo:4.1
